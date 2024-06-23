9f5838557bbaa2188447f4884dbcb6aed82579b7
suzushirou <164143331+suzushirou@users.noreply.github.com>
2024-06-23 13:53:31 +0900

    first push

---
+++ kadai_005/score.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP-BASIC-kadai005</title>
+</head>
+<body>
+  <?php
+  $score_1 = 80;
+  $score_2 = 60;
+  $score_3 = 55;
+  $score_4 = 40;
+  $score_5 = 100;
+  $score_6 = 25;
+  $score_7 = 80;
+  $score_8 = 95;
+  $score_9 = 30;
+  $score_10 = 60;
+  echo '<br>';
+  $total_score = $score_1 + $score_2 + $score_3 + $score_4 + $score_5 + $score_6 + $score_7 + $score_8 + $score_9 + $score_10;
+  echo '合計点は' . $total_score . 'です';
+  echo '<br>';
+  $average_score = $total_score / 10;
+  echo '平均点は' . $average_score . 'です';
+
+  ?>
+</body>
+</html>
