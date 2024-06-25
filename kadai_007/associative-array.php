dc5c360646c60dcb62475f597b5205661446d878
suzushirou <164143331+suzushirou@users.noreply.github.com>
2024-06-25 20:54:20 +0900

    first push

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP-BASIC-kadai007</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $vegi_data = ['name'=> 'onion', 'price'=> 200, 'weight'=> 160];
+        print_r($vegi_data);
+
+      ?>
+    </p>
+  </body>
+</html>
